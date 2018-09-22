607187afb0d2048fc9c4708139ade1be493f2be9
vladislavmakarevich <bgmakar79@gmail.com>
2018-09-22 19:48:18 +0300

    Implement func get zerosCount

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function getZerosCount(number) {
   // your implementation
+  var a= number.toString().length;
+  var res=0;
+  for(var i =1;i<a+4;i++){
+    res+=Math.floor((number/Math.pow(5,i)));
+  }
+  return (res);
 }
